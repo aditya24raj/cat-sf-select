81ec0259e2fdc2d6f76c197ca47b07bec579c07d
aditya raj <aditya2400raj@gmail.com>
2026-05-10 11:12:44 +0530

    cleanup datalist options

--- scripts/content.js
+++ scripts/content.js
@@ -8,11 +8,14 @@ function main() {
         searchElement.setAttribute('list', selectElement.id + '_list');
         searchElement.dataset.selectId = selectElement.id;
         searchElement.style.marginTop = "3px";
+        searchElement.style.width = "stretch";
+        searchElement.placeholder = "search the options";    
         searchElement.addEventListener(
             'change',
             (event) => {
+                const datalistOption = document.querySelector(`#${event.target.getAttribute('list')} option[data-text='${event.target.value}']`);
                 const selectElement = document.querySelector(`#${event.target.dataset.selectId}`);
-                selectElement.value = event.target.value;
+                selectElement.value = datalistOption.dataset.value;
                 event.target.value = null;
             }
         );
@@ -21,8 +24,9 @@ function main() {
         datalistElement.id = searchElement.getAttribute('list');
         for (const o of selectElement.options) {
             const datalistOption = document.createElement('option');
-            datalistOption.value = o.value;
-            datalistOption.text = o.text;
+            datalistOption.value = o.text;
+            datalistOption.dataset.value = o.value;
+            datalistOption.dataset.text = o.text;
             datalistElement.appendChild(datalistOption);
         }
 
